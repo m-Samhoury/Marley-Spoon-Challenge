3fba5c5c7895b75c5a306fa3500f0957cf19e865
MrSamhoury <moustafa.samhoury@gmail.com>
2019-09-10 07:13:45 +0300

    Introduced DI(Koin)

--- app/src/main/java/com/challenge/marleyspoon/MarleySpoonApplication.kt
+++ app/src/main/java/com/challenge/marleyspoon/MarleySpoonApplication.kt
@@ -1,6 +1,11 @@
 package com.challenge.marleyspoon
 
 import android.app.Application
+import com.challenge.marleyspoon.di.repositoryModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidFileProperties
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
 
 /**
  * @author moustafasamhoury
@@ -12,5 +17,15 @@ class MarleySpoonApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
+        startKoin {
+            androidContext(this@MarleySpoonApplication)
+
+            if (BuildConfig.DEBUG) {
+                androidLogger()
+            }
+            androidFileProperties()
+
+            modules(repositoryModule)
+        }
     }
 }
---
+++ app/src/main/java/com/challenge/marleyspoon/di/modules.kt
@@ -0,0 +1,27 @@
+package com.challenge.marleyspoon.di
+
+import com.challenge.marleyspoon.BuildConfig
+import com.challenge.marleyspoon.repository.Repository
+import com.challenge.marleyspoon.repository.network.MarleySpoonService
+import com.challenge.marleyspoon.repository.network.RetrofitWebServiceFactory
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.module.Module
+import org.koin.dsl.module
+
+/**
+ * @author moustafasamhoury
+ * created on Tuesday, 10 Sep, 2019
+ */
+val repositoryModule: Module = module {
+    single { RetrofitWebServiceFactory.makeHttpClient(androidContext()) }
+
+    single<MarleySpoonService> { RetrofitWebServiceFactory.makeServiceFactory(get()) }
+    single {
+        RetrofitWebServiceFactory
+            .makeRetrofit(
+                BuildConfig.BASE_API_URL,
+                get()
+            )
+    }
+    single { Repository(get()) }
+}

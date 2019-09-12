4918e16710a13a8825d86fe76efbbfab213bf23f
MrSamhoury <moustafa.samhoury@gmail.com>
2019-09-12 17:40:36 +0300

    Added Navigation component library, created the main entry for the application(MainActivity) and put RecipesListFragment as the main fragment to be shown

---
+++ app/src/main/java/com/challenge/marleyspoon/features/main/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.challenge.marleyspoon.features.recipeslist.main
+
+import com.challenge.marleyspoon.R
+import com.challenge.marleyspoon.base.MarleySpoonActivity
+
+/**
+ * @author moustafasamhoury
+ * created on Thursday, 12 Sep, 2019
+ */
+
+class MainActivity : MarleySpoonActivity(R.layout.activity_main) {
+    override fun setupViews() {
+
+    }
+}
